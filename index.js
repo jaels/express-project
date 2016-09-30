878da1e2bc211ddbe1069b334b1746fe953c5478
Yael Stein <yaelst@gmail.com>
2016-09-30 15:23:20 +0200

    express exercise with portfolio

--- index.js
+++ index.js
@@ -2,28 +2,16 @@
 
 var express = require('express');
 var app = express();
-
+var url;
 app.use(require('cookie-parser')());
 
 app.use(require('body-parser').urlencoded({
     extended: false
 }));
 
-app.get('/express-project', function(req, res, next) {
-    console.log(req.url);
-    res.redirect('/cookie');
-    next();
-});
 
 
-//app.get('/',function(req,res,next) {
-//    res.redirect('/cookie');
-//    next();
-//})
-
-
-
-app.get('/cookie', function(req,res,next) {
+app.get('/cookie', function(req,res) {
 
     res.write('<html>');
     res.write('<form method="POST">');
@@ -33,31 +21,41 @@ app.get('/cookie', function(req,res,next) {
     res.write('</form>');
     res.write('</html>');
 
-    next();
 });
+
 
 app.post('/cookie', function (req,res) {
     if (req.body["cookies"]==='yes') {
-        res.cookie('name', 'cook', {maxAge:60000})
-        res.redirect('/hello/world');
+        res.cookie('name', 'cook', {maxAge:60000});
+        res.redirect(url);
     }
     if(req.body["cookies"]==='no') {
-        res.send('Sorry, you can not use this site without accepting cookies')
+        res.send('Sorry, you can not use this site without accepting cookies');
     }
 
-})
+});
+
+
+app.get('*', function(req, res, next) {
+    if(req.cookies.name==='cook') {
+        next();
+    }
+    else{
+        url=req.url;
+        res.redirect('/cookie');}
+});
+
+
+    //app.get('/hello/world', function (req,res) {
+    //    console.log(url);
+    //    res.send('<!doctype html><title>Hello World!</title><p>Hello World!</html>');
+    //})
+
+app.use(express.static('./projects'));
 
 
 
 
-app.get('/hello/world', function (req,res) {
-    res.send('<!doctype html><title>Hello World!</title><p>Hello World!</html>');
-})
-
-app.use('/express-project',function (req,res,next) {
-    express.static('projects');
-    next();
-})
 
 
 
